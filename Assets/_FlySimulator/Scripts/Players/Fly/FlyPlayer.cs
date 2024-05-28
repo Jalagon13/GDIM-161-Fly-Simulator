13f8ac0fc6368f4dce0142c1a21f2e3197ff2ea6
Dev Patel <devp2@uci.edu>
2024-05-27 19:07:45 -0700

    Add basic fly damage

--- Assets/_FlySimulator/Scripts/Players/Fly/FlyPlayer.cs
+++ Assets/_FlySimulator/Scripts/Players/Fly/FlyPlayer.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class FlyPlayer
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializedField] private int damage = 10;
+    
+    private void Start()
     {
-
+        GameManager.Instance.JoinRound(OnRoundStart, OnRoundEnd, gameObject, true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnRoundStart()
     {
+        
+    }
 
+    private void OnRoundEnd()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameManager.Instance.DamageHuman(damage);
+        }
     }
 }
