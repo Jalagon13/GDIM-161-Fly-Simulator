1085cc9d2a5f87463971f462fea49db3378e535e
AlainaAK <alaina.klaes@gmail.com>
2024-06-03 23:53:09 -0700

    Added player name text boxes, and some player name variables in PlayerNetWork Script.
    Added text boxes for player names on flies and humans prefabs. Added a thing to populate player name when each match starts. Currently works, but has no way of players making custom names yet. Also might need to make text always face local player, like in Minecraft. (We don't want it to show through walls, though, because that would reveal human's position when hiding!)

--- Assets/_FlySimulator/netcodestuff/PlayerNetWork.cs
+++ Assets/_FlySimulator/netcodestuff/PlayerNetWork.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetWork : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI playerNameUI;
+    private NetworkObject thisNetworkObject;
+    private string thisPlayerName = "";
     // Start is called before the first frame update
     void Start()
     {
-        
+        thisNetworkObject = GetComponent<NetworkObject>();
+        PopulatePlayerName();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void PopulatePlayerName()
+    {
+        GameManager gmInst = GameManager.Instance;
+        if (gmInst != null && gmInst.LocalName != null)
+        {
+            thisPlayerName = gmInst.LocalName;
+        }
+        else
+        {
+            thisPlayerName = "Player " + (thisNetworkObject.OwnerClientId + 1);
+        }
+        playerNameUI.text = thisPlayerName;
     }
 }
